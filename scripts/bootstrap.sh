6d09e3aa83b1e181dce63ab311acb2a7e9d05469
Rtt Foo <rtt@foo.com>
2026-08-13 09:26:55 +0930

    fix build, pin repos.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,17 +5,21 @@
 set -euo pipefail
 
 # Get the restless OS project
-git clone https://github.com/djlorentz/treble_restlessos.git
-pushd treble_restlessos
+# git clone https://github.com/djlorentz/treble_restlessos.git
+# pushd treble_restlessos
 
 # remove the cawilliamson private keys
 git apply <<"EOF"
 diff --git a/configs/manifests/default.xml b/configs/manifests/default.xml
-index 6d5f39a..50f1328 100644
+index b14c3ed..1d9379c 100644
 --- a/configs/manifests/default.xml
 +++ b/configs/manifests/default.xml
-@@ -45,5 +45,5 @@
-   <project path="vendor/apn" name="android_vendor_apn" remote="lineage" revision="main" clone-depth="1" />
+@@ -21,8 +21,8 @@
+   <project path="prebuilts/vndk/v30" name="platform/prebuilts/vndk/v30" remote="aosp" revision="5f9884aa352825291757dfd6694b874ad8c1805e" clone-depth="1" />
+ 
+   <!-- lineage: apn database -->
+-  <project path="vendor/apn" name="android_vendor_apn" remote="lineage" revision="main" clone-depth="1" />
++  <project path="vendor/apn" name="android_vendor_apn" remote="lineage" revision="f61eef690eac185e6f10a227cd8ac28ab7861557" clone-depth="1" />
  
    <!-- vendor-priv -->
 -  <project path="vendor/cawilliamson-priv" name="vendor_cawilliamson-priv" remote="cawilliamson-priv" revision="main" clone-depth="1" />
@@ -23,34 +27,15 @@ index 6d5f39a..50f1328 100644
  </manifest>
 EOF
 
-# Added by us to mimic github runner behavior.
-GRAPHENEOS_RELEASE_CHANNEL="stable"
-
 # Make working directories
 mkdir --parents out/ src/ tmp/
 
 # Resolve latest GrapheneOS tag
-BUILD_DATETIME=$(date "+%s")
-BUILD_NUMBER=$(date "+%Y%m%d%H%M")
-echo "${BUILD_DATETIME}" > tmp/.build_datetime
-echo "${BUILD_NUMBER}" > tmp/.build_number
-
-echo "Fetching latest ${GRAPHENEOS_RELEASE_CHANNEL} tag from grapheneos.org/releases..."
-RELEASES_HTML=$(curl --fail --silent "https://grapheneos.org/releases")
-TAG=$(echo "${RELEASES_HTML}" \
-| grep --only-matching --perl-regexp "id=[a-z]+-${GRAPHENEOS_RELEASE_CHANNEL}><td>[^<]+</td><td><a href=#\K[0-9]{10}" \
-| sort --numeric-sort --reverse | head --lines=1)
-if [ -z "${TAG}" ]; then
-echo "ERROR: failed to extract a valid ${GRAPHENEOS_RELEASE_CHANNEL} tag"
-exit 1
-fi
-echo "Resolved latest ${GRAPHENEOS_RELEASE_CHANNEL} tag: ${TAG}"
-echo "${TAG}" > tmp/.grapheneos_tag
-
+GRAPHENEOS_TAG='2026060600'
+echo "${GRAPHENEOS_TAG}" > tmp/.grapheneos_tag
 
 # Sync sources
 pushd src
-GRAPHENEOS_TAG=$(cat ../tmp/.grapheneos_tag)
 echo "Initialising from tag: ${GRAPHENEOS_TAG}"
 repo init --depth=1 --git-lfs \
     --manifest-branch "refs/tags/${GRAPHENEOS_TAG}" \
@@ -62,6 +47,11 @@ while ! repo sync --force-sync --jobs=${SYNC_JOBS} --no-clone-bundle --no-tags; 
     echo "repo sync failed, retrying in 30s..."
     sleep 30
 done
+
+# copy the trebledroid overlays in
+echo "Syncing trebledroid resources..."
+rsync --archive --verbose ../trebledroid/ .
+echo "Done."
 
 popd # src/
 popd # treble_restlessos/
